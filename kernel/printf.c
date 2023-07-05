909ab804235985612b3531075a3fe3e95d6b17c4
Yi Liang <ylleon@umich.edu>
2023-07-05 15:23:56 -0400

    [traps] backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,17 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 fp, maxaddr, minaddr;
+  fp = r_fp();
+  maxaddr = PGROUNDUP(fp);
+  minaddr = PGROUNDDOWN(fp);
+  while(fp<maxaddr && fp > minaddr){
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  }
 }
